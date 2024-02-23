6acf53c13049644a2b3b9af94886a9aa1b8e6fdd
xun99 <96345456+xun99@users.noreply.github.com>
2024-02-23 00:14:18 +0000

    question 3.2

--- part-1/stack_rpn.hpp
+++ part-1/stack_rpn.hpp
@@ -6,5 +6,58 @@
 #include <functional>
 
 // WRITE YOUR CODE HERE
+template <typename T>
+void plus(Stack<T> &stack)
+{
+    auto val1 = stack.top();
+    stack.pop();
+    auto val2 = stack.top();
+    stack.pop();
+    stack.push(val1 + val2);
+}
+
+template <typename T>
+void minus(Stack<T> &stack)
+{
+    auto val1 = stack.top();
+    stack.pop();
+    auto val2 = stack.top();
+    stack.pop();
+    stack.push(val2 - val1);
+}
+
+template <typename T>
+void multiplies(Stack<T> &stack)
+{
+    auto val1 = stack.top();
+    stack.pop();
+    auto val2 = stack.top();
+    stack.pop();
+    stack.push(val1 * val2);
+}
+
+template <typename T>
+void divides(Stack<T> &stack)
+{
+    auto val1 = stack.top();
+    stack.pop();
+    auto val2 = stack.top();
+    stack.pop();
+    stack.push(val2 / val1);
+}
+
+// Overload the << operator to add elements to the stack
+template <typename T>
+Stack<T>& operator<<(Stack<T>& stack, void (*func)(Stack<T>&)) {
+    func(stack);
+    return stack;
+}
+
+// Overload the << operator to add elements to the stack
+template <typename T>
+Stack<T>& operator<<(Stack<T>& stack, const T& x) {
+    stack.push(x);
+    return stack;
+}
 
 #endif // __stack_rpn__
