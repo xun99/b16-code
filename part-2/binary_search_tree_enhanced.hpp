10333592b6d6004e70de10dcd38af32ef0315194
xun99 <96345456+xun99@users.noreply.github.com>
2024-02-24 12:12:40 +0000

    question 6.2

--- part-2/binary_search_tree_enhanced.hpp
+++ part-2/binary_search_tree_enhanced.hpp
@@ -6,11 +6,15 @@
 template <typename T> T bst_min(const T &tree)
 {
     // WRITE YOUR CODE HERE
+    if (!tree || !left(tree)) return tree;
+    return bst_min(left(tree));
 }
 
 template <typename T> T bst_max(const T &tree)
 {
     // WRITE YOUR CODE HERE
+    if (!tree || !right(tree)) return tree;
+    return bst_max(right(tree));
 }
 
-#endif // __binary_saerch_tree_enhanced__
+#endif // __binary_search_tree_enhanced__
