12685ef89374066ccf32c05fd6ee5223bd873344
xun99 <96345456+xun99@users.noreply.github.com>
2024-02-24 01:06:32 +0000

    question 6.1

--- part-2/binary_tree_enhanced.hpp
+++ part-2/binary_tree_enhanced.hpp
@@ -12,6 +12,7 @@ template <typename V> struct BinaryTreeEnhanced {
     std::unique_ptr<BinaryTreeEnhanced<V>> _right;
 
     // WRITE YOUR CODE HERE
+    BinaryTreeEnhanced<V>* _parent; // add a pointer to the parent node
 
     friend V &value(BinaryTreeEnhanced *t) { return t->_value; }
     friend const V &value(const BinaryTreeEnhanced *t)
@@ -29,6 +30,7 @@ template <typename V> struct BinaryTreeEnhanced {
     friend BinaryTreeEnhanced *parent(const BinaryTreeEnhanced *t)
     {
         // WRITE YOUR CODE HERE
+        return t->_parent;
     }
 };
 
@@ -40,6 +42,18 @@ make_binary_tree_enhanced(const V &value,
                           std::unique_ptr<BinaryTreeEnhanced<V>> r)
 {
     // WRITE YOUR CODE HERE
+    auto tree = std::unique_ptr<BinaryTreeEnhanced<V>>{
+        new BinaryTreeEnhanced<V>{
+            value,
+            std::move(l),
+            std::move(r)
+        }
+    };
+    // link children to parent
+    if (tree->_left.get()) tree->_left.get()->_parent = tree.get();
+    if (tree->_right.get()) tree->_right.get()->_parent = tree.get();
+
+    return tree;
 }
 
 #endif // __binary_tree_enhanced__
