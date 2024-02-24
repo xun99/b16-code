3f621a5ea872e0394b4e1de9f782de490276e1e2
xun99 <96345456+xun99@users.noreply.github.com>
2024-02-24 00:38:10 +0000

    question 4

--- part-1/list_enhanced.hpp
+++ part-1/list_enhanced.hpp
@@ -8,9 +8,15 @@
 template <typename T> void list_delete_after(Node<T> *node)
 {
     // WRITE YOUR CODE HERE
+
+
     assert(node);
     if (node->next){
-        node->next = std::move(node->next->next);
+        std::unique_ptr<Node<T>>& nextNodePtr = node->next; // Get a reference to the unique_ptr holding the next node
+        std::unique_ptr<Node<T>> removedNode = std::move(nextNodePtr); // Move ownership of the next node to a local unique_ptr
+        node->next = std::move(removedNode->next); // Update the next pointer of the given node to skip over the removed node
+        // or we can use 
+        // node->next = std::move(node->next->next);
     }
 }
 
