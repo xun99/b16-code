50fc740ed92a13b3aa20a7394a463976417bad0e
xun99 <96345456+xun99@users.noreply.github.com>
2024-02-27 01:41:07 +0000

    question 9

--- part-4/shortest_paths_sparse.cpp
+++ part-4/shortest_paths_sparse.cpp
@@ -6,10 +6,23 @@
 std::vector<hop_t> bellman_ford(const SparseGraph &graph, const int source,
                                 bool &has_negative_cycle)
 {
-    const int V = static_cast<int>(graph.size());
+    const int V = static_cast<int>(graph.size()); //number of nodes
     auto DP = std::vector<hop_t>(V, {inf, -1});
+    DP[source] = {0, source};
 
     // WRITE YOUR CODE HERE
+    for (int i = 0; i < V-1; i++) {
+        for (int u = 0; u < V; u++) {
+            for (const auto &edge : graph[u]) {
+                // path relaxation: source to v versus source to u to v
+                auto distance = DP[edge.vertex].weight; // from source to v (edge.vertex) 
+                auto alt_distance = DP[u].weight + edge.weight; // from source to u to v
+                if (alt_distance < distance) {
+                    DP[edge.vertex] = {alt_distance, u};
+                }
+            }
+        }
+    }
 
     return DP;
 }
@@ -28,6 +41,15 @@ std::vector<hop_t> dijkstra(const SparseGraph &graph, const int source)
     auto DP = std::vector<hop_t>(graph.size(), {inf, -1});
 
     // WRITE YOUR CODE HERE
+    auto queue = std::vector<triplet_t>{};
+    auto compare = [](const triplet_t &lhs, const triplet_t &rhs) {
+        return lhs.d < rhs.d; 
+    };
+    priority_enqueue(queue, {0, source, source}, compare);
+
+    while(!queue.empty()) {
+        
+    }
 
     return DP;
 }
