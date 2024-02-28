beb456261e2106de0cb88f04ab9629364e0c003a
xun99 <96345456+xun99@users.noreply.github.com>
2024-02-28 00:14:28 +0000

    question 9

--- part-4/shortest_paths_decode.hpp
+++ part-4/shortest_paths_decode.hpp
@@ -8,6 +8,19 @@
 inline std::vector<int> decode(const std::vector<hop_t> &DP, int v)
 {
     // WRITE YOUR CODE HERE
+
+    // if cannot go from u to v
+    if (DP[v].vertex == -1) return {};
+
+    std::vector<int> path{v};
+    while(true) {
+        auto prev = DP[v].vertex;
+        if (prev == v || prev == -1) break;
+        v = prev;
+        path.push_back(v);
+    }
+    std::reverse(begin(path), end(path));
+    return path;
 }
 
 #endif // __shortest_paths_decode__
--- part-4/shortest_paths_sparse.cpp
+++ part-4/shortest_paths_sparse.cpp
@@ -45,14 +45,61 @@ std::vector<hop_t> dijkstra(const SparseGraph &graph, const int source)
     auto DP = std::vector<hop_t>(graph.size(), {inf, -1});
 
     // WRITE YOUR CODE HERE
+
+    // method 1
+    DP[source].weight = 0;
     auto queue = std::vector<triplet_t>{};
     auto compare = [](const triplet_t &lhs, const triplet_t &rhs) {
         return lhs.d < rhs.d; 
-    };
+    }; // lambda function
     priority_enqueue(queue, {0, source, source}, compare);
+    while(!queue.empty()) {
+        auto v_star = queue[0].v;
+        auto d_star = queue[0].d;
+        auto r_star = queue[0].r;
+        // dequeue {d_star, r_star, v_star}
+        priority_dequeue(queue, compare);
+        for (const auto &edge : graph[v_star]) {
+            auto alt_distance = d_star + edge.weight; // D_v* + w(v*,v)
+            auto distance = DP[edge.vertex].weight; // D_v, where v is edge.vertex;
+            // D_v* + w(v*,v) < D_v, path relaxation
+            // source to v_* to v is shorter than source to v
+            if (alt_distance<distance) {
+                DP[edge.vertex] = {alt_distance, v_star};
+                priority_enqueue(queue, {alt_distance, v_star, edge.vertex}, compare);
+            }
+        }
+    }
+
+    // method 2
+    // auto queue = std::vector<triplet_t>{};
+    // // auto compare = std::less<triplet_t>{};
+    // auto compare = [](const triplet_t &lhs, const triplet_t &rhs) {
+    //     return lhs.d < rhs.d; 
+    // }; // lambda function
+
+    // priority_enqueue(queue, {0, source, source}, compare);
 
     // while(!queue.empty()) {
-        
+    //     auto v_star = queue[0].v;
+    //     auto d_star = queue[0].d;
+    //     auto r_star = queue[0].r;
+
+    //     // dequeue {d_star, r_star, v_star}
+    //     priority_dequeue(queue, compare);
+
+    //     if (DP[v_star].weight <= d_star) { 
+    //         continue;
+    //     }
+    //     DP[v_star] = {d_star, r_star};
+
+    //     for (const auto &edge : graph[v_star]) {
+    //         auto d = d_star + edge.weight;
+    //         if (!std::isfinite(d)) {
+    //             continue;
+    //         }
+    //         priority_enqueue(queue, {d, v_star, edge.vertex}, compare);
+    //     }
     // }
 
     return DP;
